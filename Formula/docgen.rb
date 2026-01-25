d4fdcbd8f7e6c1df1c90ce1cb3b9fd4dacb11e63
Jörn Seidel <joern.seidel@casoon.de>
2026-01-25 21:45:45 +0100

    Update Formula for v0.4.11
    Fixes:
    - Concept template logo displays correctly
    - Concept footer: simple one-line layout (like credentials)
    - Logo pre-loading works in package-import mode

--- Formula/docgen.rb
+++ Formula/docgen.rb
@@ -6,28 +6,28 @@
 class Docgen < Formula
   desc "CLI tool for generating professional business documents with Typst"
   homepage "https://github.com/casoon/typst-business-templates"
-  version "0.4.10"
+  version "0.4.11"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/casoon/typst-business-templates/releases/download/v#{version}/docgen-aarch64-apple-darwin.tar.gz"
-      sha256 "7720466d603e9aecfa477d8c6f5b479816aa1a34dcf74e703ed964f839335c5f"
+      sha256 "d792faffc7824a0861683eda5f42a1f5c79b088ac8b4b2b6f0641f3bb517aeab"
     end
     on_intel do
       url "https://github.com/casoon/typst-business-templates/releases/download/v#{version}/docgen-x86_64-apple-darwin.tar.gz"
-      sha256 "249c46166e62273d6749f672714d247f7daadc55f4e3c324de238f798fdd5107"
+      sha256 "0643253d87a5ad4617de0e7d676705d32b9133bf0b30673d1501c5b7636d64f6"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/casoon/typst-business-templates/releases/download/v#{version}/docgen-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "97f2842c28ac9af27e14a45ece2ee9c61e6492e7969a35e162036b8144474638"
+      sha256 "055a51449bba3c7451c3da003c662cd7c25d2398e330c9d362aeaf5e3576ebfa"
     end
     on_intel do
       url "https://github.com/casoon/typst-business-templates/releases/download/v#{version}/docgen-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "06258a9234abe8493adace0fd25c6febcf8fd158456d3271b78871a8cdd2b012"
+      sha256 "8857625106f72b2ec3cc62e7557434676512b32fedf1394ec2bd0e9b31000e95"
     end
   end
 
