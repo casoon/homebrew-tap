13088cbc23022713885494e104133f78c44d6515
Jörn Seidel <joern.seidel@casoon.de>
2026-01-25 23:49:43 +0100

    Update docgen to v0.5.2
    - Fix JSON workflow template paths
    - Fix offer template syntax error
    - Fix docgen init template system

--- Formula/docgen.rb
+++ Formula/docgen.rb
@@ -6,28 +6,28 @@
 class Docgen < Formula
   desc "CLI tool for generating professional business documents with Typst"
   homepage "https://github.com/casoon/typst-business-templates"
-  version "0.5.0"
+  version "0.5.2"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/casoon/typst-business-templates/releases/download/v#{version}/docgen-aarch64-apple-darwin.tar.gz"
-      sha256 "f23a12beb940954a619caa3e6d3370e253c7890e9d51906d643ffd76990d1f69"
+      sha256 "47cc0eab53576d861389daa6f94bcb8cb3f8fe8b47347f65192a36ee0c9c7f4b"
     end
     on_intel do
       url "https://github.com/casoon/typst-business-templates/releases/download/v#{version}/docgen-x86_64-apple-darwin.tar.gz"
-      sha256 "03bc40ae085d06f2406bb44225228c708d3f1ac5db6eb80573774e6c47b90684"
+      sha256 "408b672a4e5d2f5937ad204314723fdda8d55e4c9cbe0dbf848ddd50b3c1b2dc"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/casoon/typst-business-templates/releases/download/v#{version}/docgen-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "c052f5df6e6daa4fe57b0b437f153058c6a27bb91a809c38474a11f6ee52bc27"
+      sha256 "63c18fa55eecb704c6b787f99b92b1791f8a3cb4668ab5a9daa5d559643bdd58"
     end
     on_intel do
       url "https://github.com/casoon/typst-business-templates/releases/download/v#{version}/docgen-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "57e44e86e8f508f7132cbb9d7f9893ad6e9bfb3e18e41258c433ac924e63a2e5"
+      sha256 "7e8a9d2c6ba57cf1daf0acf9dc3b2a6fc6da5a6aab99cff3a66c97bfe608809b"
     end
   end
 
