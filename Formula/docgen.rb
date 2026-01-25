7b845d1b91992e94dbf2080e2c03fecdaaa19c30
Jörn Seidel <joern.seidel@casoon.de>
2026-01-25 17:54:20 +0100

    Update Formula checksums for v0.4.9 (with correct version string)
    Updated checksums after rebuilding binaries with version 0.4.9 in Cargo.toml

--- Formula/docgen.rb
+++ Formula/docgen.rb
@@ -12,22 +12,22 @@ class Docgen < Formula
   on_macos do
     on_arm do
       url "https://github.com/casoon/typst-business-templates/releases/download/v#{version}/docgen-aarch64-apple-darwin.tar.gz"
-      sha256 "683373ea65a3e5192ea6f43324d6796063ad6817d6bd010e6c2dbea20bc4f8bf"
+      sha256 "e05c3a2896d8be82fc9a2a1c3b68482e746720970900902a709290e8373d2d4c"
     end
     on_intel do
       url "https://github.com/casoon/typst-business-templates/releases/download/v#{version}/docgen-x86_64-apple-darwin.tar.gz"
-      sha256 "8cd75df65841639e05c3744a183c8a0aae44ddb1128f17d2a61e56e6e0ec0ebd"
+      sha256 "b01d446efe0fb7f00411c75cb642df26be8ff196064b1b0731e929ce98710ff3"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/casoon/typst-business-templates/releases/download/v#{version}/docgen-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "f0e895371571ca72d5655f0ef0392a9abddda84436e6d0b5364e5ff67b51f454"
+      sha256 "17b31ad347956c930536408fde269f3208cb3fa75feba4debaa2bc7fdc484abc"
     end
     on_intel do
       url "https://github.com/casoon/typst-business-templates/releases/download/v#{version}/docgen-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "56db93203f32814a4a6a7e26ae7b477a52efc4ed458e4bd58304544c16453663"
+      sha256 "46c2168c177361eba21b88625010e43d56e7fe9190d31f15e3ec21d7f47116a9"
     end
   end
 
