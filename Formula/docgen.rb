d5cb7214405619231d0a4fb08e7c5ee12d681860
Jörn Seidel <joern.seidel@casoon.de>
2026-01-27 11:06:26 +0100

    Fix checksums for v0.6.6

--- Formula/docgen.rb
+++ Formula/docgen.rb
@@ -12,22 +12,22 @@ class Docgen < Formula
   on_macos do
     on_arm do
       url "https://github.com/casoon/typst-business-templates/releases/download/v#{version}/docgen-aarch64-apple-darwin.tar.gz"
-      sha256 "5c21c12b99eaf0df17ca3a8fdff7e4cea4f25d97d2ec8a8f6e57de8b8c34de87"
+      sha256 "ff8f97cd11f3d7dbc476f55565f3997ccf094df18bb52fa882a1c58a794e4a73"
     end
     on_intel do
       url "https://github.com/casoon/typst-business-templates/releases/download/v#{version}/docgen-x86_64-apple-darwin.tar.gz"
-      sha256 "b82bd9fc4f30bdc39fb21ee3aba3b8b7d76a17ccf9fb01a3e009d22fd0bd3c33"
+      sha256 "4b66ab96da00b92d0bc875c8f3d17f7679b9992d0529ae3c436c61ba40c8f7fb"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/casoon/typst-business-templates/releases/download/v#{version}/docgen-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "4dd1b4a87be5b73c4a77d02f0d2ba4fde6e1e1e1e00cf62a6ed1c36b9a24a60d"
+      sha256 "7abbbee9eb5a7fd3fe9041517df71ab2fe16c847d778cca0c5e640032f3ea31d"
     end
     on_intel do
       url "https://github.com/casoon/typst-business-templates/releases/download/v#{version}/docgen-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "8b8e85ddce73f33edfe1c3b0c9c6dff6c9b74f42a52dae6ffcacd1c33aa04cf4"
+      sha256 "c369103de70c8c02fbffb0397a309533b4f2950973cbed5e73482f1ea59d44c0"
     end
   end
 
